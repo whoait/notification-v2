32864cb294dd155fc656cecc9d6d699958da34e2
Dat Ho <hominhdatdn@gmail.com>
2017-11-03 17:01:33 +0700

    - Import into database from notification file (with ad and news element)

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const models = require('../models');
 const util = require('../util/util');
 const appConst = require('../util/const');
 const base64 = require('base-64');
+const _ = require('underscore');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -38,16 +39,125 @@ router.post('/uploadNotificationFile', function (req, res) {
     const bindVersion = req.body.bindVersion;
     const notificationData = JSON.parse(base64.decode(req.body.files[0].src.split(',')[1]));
 
-    checkAdElement(notificationData, bindVersion)
-        .then(() => {
-            console.log('all done');
-            res.send(util.responseSuccess());
-        })
-        .catch(() => {
-            res.send(util.responseInvalidJsonFormat());
-        });
+    // checkAdElement(notificationData, bindVersion)
+    //     .then(() => {
+    //         checkNewsElement(notificationData, bindVersion)
+    //     })
+    //     .then(() => {
+    //         console.log('ALL DONE');
+    //         res.send(util.responseSuccess());
+    //     })
+    //     .catch(() => {
+    //         res.send(util.responseInvalidJsonFormat());
+    //     });
+    const p1 = checkAdElement(notificationData, bindVersion);
+    const p2 = checkNewsListElement(notificationData, bindVersion);
+    Promise.all([p1, p2]).then(() => {
+        console.log('ALL DONE');
+        res.send(util.responseSuccess());
+    })
 });
 
+function checkNewsListElement(notificationData, bindVersion) {
+    return new Promise((resolve, reject) => {
+        if (notificationData.hasOwnProperty('news')) {
+            const newsList = notificationData.news;
+            if (util.isEmpty(newsList)) {
+                resolve();
+            }
+            else {
+                const promises = _.map(newsList, (news) => {
+                    return checkEachNewsElement(news, bindVersion);
+                });
+                Promise.all(promises).then(() => {
+                    console.log('all promise done');
+                    resolve();
+                });
+            }
+        }
+        else {
+            reject();
+        }
+    });
+}
+
+function checkEachNewsElement(newsElement, bindVersion) {
+    return new Promise((resolve, reject) => {
+        const newsTitle = newsElement.title;
+        new Promise((cResolve, cReject) => {
+            const item = models.bind_notification.find({
+                where: {
+                    message_type: 'news',
+                    title: newsTitle
+                },
+                include: [
+                    {
+                        model: models.bind_notification_detail,
+                        where: {
+                            delete_flag: false,
+                        }
+                    }
+                ]
+            });
+            cResolve(item);
+        }).then((item) => {
+            if (util.isEmpty(item)) {
+                // If news not existed, add new news element
+                return addNewsElement(newsTitle, bindVersion);
+            }
+            else {
+                // If news existed, update bind version.
+                return updateNewsElementWithBindVersion(item, bindVersion);
+            }
+        }).then((item) => {
+            console.log(`news element id = ${item.id}`);
+            console.log(newsElement.list);
+            resolve();
+        });
+    });
+}
+
+function addNewsElement(newsTitle, bindVersion) {
+    return new Promise((resolve, reject) => {
+        new Promise((cResolve, cReject) => {
+            const bind_notification = models.bind_notification.create(
+                {
+                    message_type: 'news',
+                    title: newsTitle
+                }
+            );
+            cResolve(bind_notification);
+        }).then((bind_notification) => {
+            const parentId = bind_notification.id;
+            return models.bind_notification_detail.create(
+                {
+                    parent_id: parentId,
+                    [bindVersion]: true,
+                    status: appConst.STATUS_PUBLISHING
+                }
+            );
+        }).then((item) => {
+            resolve(item);
+        });
+    });
+}
+
+function updateNewsElementWithBindVersion(item, bindVersion) {
+    console.log(`update new element with bind version ${bindVersion} and parent_id = ${item.id}`);
+    return new Promise((resolve, reject) => {
+        models.bind_notification_detail.update(
+            {
+                [bindVersion]: true
+            },
+            {
+                where: {parent_id: item.id},
+                delete_flag: true,
+                status: appConst.STATUS_PUBLISHING
+            }
+        );
+        resolve(item);
+    });
+}
 
 function checkAdElement(notificationData, bindVersion) {
     return new Promise((resolve, reject) => {
@@ -71,7 +181,6 @@ function checkAdElement(notificationData, bindVersion) {
                         }
                     ]
                 });
-                console.log('check done');
                 cResolve(item);
             }).then((item) => {
                 if (util.isEmpty(item)) {
@@ -92,7 +201,7 @@ function checkAdElement(notificationData, bindVersion) {
 }
 
 function updateAdWithBindVersion(item, bindVersion) {
-    console.log(`update with bind version ${bindVersion} and parent_id = ${item.id}`);
+    console.log(`update ad element with bind version ${bindVersion} and parent_id = ${item.id}`);
     return new Promise((resolve, reject) => {
         models.bind_notification_detail.update(
             {
